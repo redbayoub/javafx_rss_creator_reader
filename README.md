9116fcb8b0abe37117ace5225537719918cc0453
Red Ayoub <redayoub47@gmail.com>
2019-07-15 19:22:42 +0100

    upload files

---
+++ README.md
@@ -0,0 +1,1 @@
+# JavaFx RSS Creator Reader
---
+++ test/test.java
@@ -0,0 +1,27 @@
+
+import java.io.File;
+import java.util.ArrayList;
+import javafx.scene.control.TextInputDialog;
+import rsscreator.ui.resourses.FeedMessage;
+import rsscreator.ui.resourses.filesHandler;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author redayoub
+ */
+public class test {
+    public static void main(String[] args) throws Exception {
+//        ArrayList<FeedMessage> listMsg=filesHandler.importFrom(new File("/home/redayoub/Desktop/new file.txt"));
+//        for(FeedMessage msg:listMsg){
+//            System.out.println(msg.toString());
+//        }
+
+        
+    }
+}
